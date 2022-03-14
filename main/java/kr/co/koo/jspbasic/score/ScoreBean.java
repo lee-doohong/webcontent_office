049095c308e62365d0b89a7b270b0f29585f673d
lee-doohong <lovelyldh@naver.com>
2022-03-14 19:30:37 +0900

    20220314

---
+++ main/java/kr/co/koo/jspbasic/score/ScoreBean.java
@@ -0,0 +1,10 @@
+package kr.co.koo.jspbasic.score;
+
+public class ScoreBean {
+
+	/*
+	 # 여기에는 국어점수(kor:int), 영어점수(eng:int),
+	 수학점수(math:int), 총점(total:int), 평균(average:double)
+	 총 5개의 데이터를 관리하는 자바빈 클래스를 구성하세요
+	 */
+}
